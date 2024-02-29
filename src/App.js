183b7c32f5558a5c513b82c0889fd778af088cbc
LokeshATR <lokesh.bhavsar@antiersolutions.com>
2024-02-29 15:27:55 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import {
+  DynamicWidget,
+  DynamicContextProvider,
+} from "@dynamic-labs/sdk-react-core";
+import { EthersExtension } from "@dynamic-labs/ethers-v5";
+
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { AlgorandWalletConnectors } from "@dynamic-labs/algorand";
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
+import { FlowWalletConnectors } from "@dynamic-labs/flow";
+import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
+import { CosmosWalletConnectors } from "@dynamic-labs/cosmos";
+import { MagicWalletConnectors } from "@dynamic-labs/magic";
+import { BloctoEvmWalletConnectors } from "@dynamic-labs/blocto-evm";
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <DynamicContextProvider
+        settings={{
+          environmentId: "1c52044f-69f1-437b-b8b3-05c42ab5307d",
+          walletConnectorExtensions: [EthersExtension],
+          walletConnectors: [
+            EthereumWalletConnectors,
+            AlgorandWalletConnectors,
+            SolanaWalletConnectors,
+            FlowWalletConnectors,
+            StarknetWalletConnectors,
+            CosmosWalletConnectors,
+            MagicWalletConnectors,
+            BloctoEvmWalletConnectors,
+          ],
+        }}
+      >
+        <DynamicWidget />
+      </DynamicContextProvider>
     </div>
   );
 }
