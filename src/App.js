b4a1693db619014abbdd43e7e0d5f80aabffcc62
LokeshATR <lokesh.bhavsar@antiersolutions.com>
2024-02-29 14:58:41 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import "./App.css";
-
-import {
-  DynamicWidget,
-  DynamicContextProvider,
-} from "@dynamic-labs/sdk-react-core";
-import { EthersExtension } from "@dynamic-labs/ethers-v5";
-
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { AlgorandWalletConnectors } from "@dynamic-labs/algorand";
-import { SolanaWalletConnectors } from "@dynamic-labs/solana";
-import { FlowWalletConnectors } from "@dynamic-labs/flow";
-import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
-import { CosmosWalletConnectors } from "@dynamic-labs/cosmos";
-import { MagicWalletConnectors } from "@dynamic-labs/magic";
-import { BloctoEvmWalletConnectors } from "@dynamic-labs/blocto-evm";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-   hello
+      hello world
     </div>
   );
 }
